fee3f5f7762f30e227b07dcabda8262fbbc63c62
katuchen <kate02kuz@icloud.com>
2024-07-06 13:49:41 +0300

    Initial Commit

---
+++ CryptoShit/CryptoShitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoShitApp.swift
+//  CryptoShit
+//
+//  Created by Екатерина Кузмичева on 06.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoShitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
